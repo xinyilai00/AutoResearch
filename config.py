8c2e7d9c92883c6c6aba9ecfdd68cd563bbe049e
Jerbear <jerryrchang729@gmail.com>
2026-06-16 17:05:52 +0800

    add config and env setup

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv(dotenv_path=Path(__file__).parent / ".env")
+
+BASE_URL = os.getenv("BASE_URL")
+API_KEY = os.getenv("API_KEY")
+AGENT_ID = os.getenv("AGENT_ID")
+PRINCIPAL_ID = os.getenv("PRINCIPAL_ID")
